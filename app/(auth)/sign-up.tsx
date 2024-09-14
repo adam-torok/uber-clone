b3351d99d956d36fe9ce5ab683e00076b94209ec
Török Ádám <adam-torok@outlook.hu>
2024-09-14 20:52:13 +0200

    Working on welcome screen

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignUp = () => {
+  return (
+    <SafeAreaView>
+      <Text className={"p-5 pt-0"}>Sign Up</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
