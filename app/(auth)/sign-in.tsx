fe91447c15867c0dae3454481bc7f3cb52a5c32d
Török Ádám <adam-torok@outlook.hu>
2024-10-19 00:50:28 +0200

    Adding clerk token cache, tabs to switch screens

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,21 +1,44 @@
 import { icons, images } from "@/constants";
 import { ScrollView, Text, Image, View, TouchableOpacity } from "react-native";
 import InputField from "@/components/InputField";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Link, router } from "expo-router";
 import CustomButton from "@/components/CustomButton";
 import OAuth from "@/components/Oath";
-import { useSignUp } from "@clerk/clerk-expo";
+import { useSignIn } from "@clerk/clerk-expo";
+import { Link, useRouter } from "expo-router";
 
 const SignIn = () => {
+  const { signIn, setActive, isLoaded } = useSignIn();
+  const router = useRouter();
+
   const [form, setForm] = useState({
     name: "",
     email: "",
     password: "",
   });
 
-  const onSignInPress = async () => {};
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/");
+      } else {
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [isLoaded, form.email, form.password]);
 
   return (
     <ScrollView className="flex-1 bg-white">
